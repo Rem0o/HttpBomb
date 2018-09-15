4b7bc7b5f937cfb2329f8ffc8749de1fbb583dd4
Rem0o <rem54321@hotmail.com>
2018-09-14 23:07:19 -0400

    Validation matches message

--- OptionValidators/DurationValidator.cs
+++ OptionValidators/DurationValidator.cs
@@ -8,7 +8,7 @@ namespace HttpBomb.OptionValidators
     {
         public ValidationResult GetValidationResult(CommandOption option, ValidationContext context)
         {
-            if (option.HasValue() && int.TryParse(option.Value(), out var time) && time > 1 && time < 3600)
+            if (option.HasValue() && int.TryParse(option.Value(), out var time) && time >= 1 && time <= 3600)
                 return ValidationResult.Success;
             else
                 return new ValidationResult($"The value for -{option.ShortName}|--{option.LongName} is required and must be between 1 and 3600 seconds.");
